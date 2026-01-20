251d0d7cf4483aff3ab50d523ff719470eff7170
abc100 <abccba111307@gmail.com>
2026-01-21 01:39:33 +0900

    fix: footer, admin page style
    -footer 띄워지게 끔 style 변경
    -admin page 내 리스트들이 최소한의 높이 가지도록 스타일 변경

--- src/pages/borrowList.tsx
+++ src/pages/borrowList.tsx
@@ -61,17 +61,19 @@ useEffect(() => {
         <SearchBar handleSearch={handleSearch} State="user" />
         <CheckBox checked={check} onChange={toggleCheck} />
         <CategoryBox />
-        {listData.map((e) => (
-          <InfoBox
-            book_name={e.book_name}
-            registration_number={e.registration_number}
-            nick_name={e.nick_name}
-            return_date={e.return_date}
-            overdue_status={e.overdue_status}
-            allow={e.allow}
-            day={e.day}
-          />
-        ))}
+        <BookContinaer>
+          {listData.map((e) => (
+            <InfoBox
+              book_name={e.book_name}
+              registration_number={e.registration_number}
+              nick_name={e.nick_name}
+              return_date={e.return_date}
+              overdue_status={e.overdue_status}
+              allow={e.allow}
+              day={e.day}
+            />
+          ))}
+        </BookContinaer>
       </Container>
       <PaginationBar 
         pageCount={pageCount}
@@ -86,4 +88,10 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+const BookContinaer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 249px;
 `
